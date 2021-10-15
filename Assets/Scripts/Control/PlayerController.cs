94add7b62d2281bc88d75af9474c1c96914dd24a
Jathro <pete.slusarz@gmail.com>
2021-10-15 19:39:01 -0400

    cursor and UI updates

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -22,6 +22,7 @@ namespace RPG.Control
 
         [SerializeField] CursorMapping[] cursorMappings = null;
         [SerializeField] float maxNavMeshProjectionDistance = 1.0f;
+        [SerializeField] float maxNavPathLength = 40f;
 
         private void Awake()
         {
@@ -112,7 +113,24 @@ namespace RPG.Control
             if (!hasCastToNavMesh) { return false; }
 
             target = navMeshHit.position;
+
+            NavMeshPath path = new NavMeshPath();
+            bool hasPath = NavMesh.CalculatePath(transform.position, target, NavMesh.AllAreas, path);
+            if (!hasPath) { return false; }
+            if (path.status != NavMeshPathStatus.PathComplete) { return false; }
+            if (GetPathLength(path) > maxNavPathLength) { return false; }
             return true;
+        }
+
+        private float GetPathLength(NavMeshPath path)
+        {
+            float pathLength = 0;
+            if (path.corners.Length < 2) { return pathLength; }
+            for (int i = 0; i < path.corners.Length - 1; i++)
+            {
+                pathLength += Vector3.Distance(path.corners[i], path.corners[i + 1]);
+            }
+            return pathLength;
         }
 
         private void SetCursor(CursorType type)
